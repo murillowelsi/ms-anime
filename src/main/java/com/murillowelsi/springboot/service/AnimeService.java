50afe22f0750f2a8980b8ea9ca05e0f1b0404fc2
Murillo Welsi <murillo.welsi@gmail.com>
2022-01-22 18:11:33 +0000

    Added findById

--- src/main/java/com/murillowelsi/springboot/service/AnimeService.java
+++ src/main/java/com/murillowelsi/springboot/service/AnimeService.java
@@ -1,13 +1,25 @@
 package com.murillowelsi.springboot.service;
 
 import com.murillowelsi.springboot.domain.AnimeDomain;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AnimeService {
+    private final List<AnimeDomain> animes = List.of(new AnimeDomain(1L,"DBZ"), new AnimeDomain(2L, "Sakura"));
+
     public List<AnimeDomain> listAll(){
-        return List.of(new AnimeDomain(1L,"DBZ"), new AnimeDomain(2L, "Sakura"));
+        return animes;
+    }
+
+    public AnimeDomain findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found."));
     }
 }
