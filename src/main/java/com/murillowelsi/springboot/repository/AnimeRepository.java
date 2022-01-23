1cca41f050f318e6b1f3dd9ef99fe262fb90d796
Murillo Welsi <murillo.welsi@gmail.com>
2022-01-23 08:10:13 +0000

    extended JpaRepository class

--- src/main/java/com/murillowelsi/springboot/repository/AnimeRepository.java
+++ src/main/java/com/murillowelsi/springboot/repository/AnimeRepository.java
@@ -1,9 +1,10 @@
 package com.murillowelsi.springboot.repository;
 
 import com.murillowelsi.springboot.model.domain.AnimeDomain;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AnimeRepository {
-    List<AnimeDomain> listAll();
+public interface AnimeRepository extends JpaRepository<AnimeDomain, Long> {
+    List<AnimeDomain> findAll();
 }
