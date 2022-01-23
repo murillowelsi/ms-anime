ce82e90dde0089540108c79957bc56bff6c53a89
Murillo Welsi <murillo.welsi@gmail.com>
2022-01-23 07:19:13 +0000

    added database notations

--- src/main/java/com/murillowelsi/springboot/model/domain/AnimeDomain.java
+++ src/main/java/com/murillowelsi/springboot/model/domain/AnimeDomain.java
@@ -2,12 +2,21 @@ package com.murillowelsi.springboot.model.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class AnimeDomain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
